39a2e26f624b32aff13152e2092bedabc71ebf88
hlevente654 <hlevente654@gmail.com>
2021-02-17 10:21:10 +0100

    Tesztauzem

---
+++ 20210217/web/tesztauzem.js
@@ -0,0 +1,7 @@
+﻿function gomb(){
+    let kapott = document.getElementById("jatek-valasz").value;
+    if(kapott != ""){
+        alert("Köszönjük hogy részt vesz a játékunkban")
+        document.getElementById("jatek-valasz").value = "";
+    }
+}
